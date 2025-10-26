23830cf99aa19d680c116c657bb36d33d365bddc
innams87-code <innams87@gmail.com>
2025-10-26 09:25:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Auto year in footer
+console.log("JS loaded");
+console.log("items found:", document.querySelectorAll("#menu li").length);// Auto year in footer
 const y = document.getElementById("year");
 if (y) y.textContent = new Date().getFullYear();
 
